fb08d561016a4d071119b7eb3eadb2122ec17d42
Andrew-S-Nelson <andrewnelson2596@gmail.com>
2024-07-16 15:09:54 -0400

    Update binary_conversions.py

--- binary_conversions.py
+++ binary_conversions.py
@@ -1,8 +1,8 @@
-# converts an ascii character (formatted as a string) to a list of 8 binary integers
+# converts an ascii character (formatted as a string) to an 8-bit binary list
 def char_to_bin(character):
     return list(map(int, ''.join(format(ord(i), '08b') for i in character)))
 
-# converts and integer to a list of 8 binary integers
+# converts and integer to an 8-bit binary list
 def int_to_bin(decimal):
     binary = list(map(int, "{0:b}".format(decimal)))
     outlist = []
