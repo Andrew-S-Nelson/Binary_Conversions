458467187623aff091a7ea3f48d0830c13e2460d
Andrew-S-Nelson <andrewnelson2596@gmail.com>
2024-07-16 15:42:41 -0400

    Update binary_conversions.py

--- binary_conversions.py
+++ binary_conversions.py
@@ -1,6 +1,23 @@
+# converts an 8-bit binary list to an integer
+def bin_to_dec(binary_list):
+    multiplier = 128
+    outdec = 0
+    for i in range(8):
+        outdec += binary_list[i] * multiplier
+        multiplier = multiplier // 2
+    return outdec
+
 # converts an ascii character (formatted as a string) to an 8-bit binary list
 def char_to_bin(character):
     return list(map(int, ''.join(format(ord(i), '08b') for i in character)))
+    
+# converts a string into a list of 8-bit binary lists
+def str_to_binlist(string):
+    char_list = list(string)
+    bin_list = []
+    for character in char_list:
+        bin_list.append(char_to_bin(character))
+    return bin_list
 
 # converts and integer to an 8-bit binary list
 def int_to_bin(decimal):
@@ -12,11 +29,13 @@ def int_to_bin(decimal):
         outlist.append(i)
     return outlist
 
-# converts an 8-bit binary list to an integer
-def bin_to_dec(binary_list):
-    multiplier = 128
-    outdec = 0
-    for i in range(8):
-        outdec += binary_list[i] * multiplier
-        multiplier = multiplier // 2
-    return outdec
+# converts an 8-bit binary list to a character
+def bin_to_char(binary_list):
+    return chr(bin_to_dec(binary_list))
+
+# converts a list of 8-bit binary lists into a string
+def binlist_to_str(binary_list_list):
+    outstr = ""
+    for binary_list in binary_list_list:
+        outstr += bin_to_char(binary_list)
+    return outstr
